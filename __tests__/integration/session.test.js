b787c217a82a3b85552859c88e5bc5aa14fc6643
elvismariel <elvismariel@gmail.com>
2019-11-04 17:18:04 -0300

    Configuracao do Jest para rodar os testes no NodeJs

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,3 +1,4 @@
+/*
 describe("Authentication", () => {
     it("shold sum two numbers", () => {
         const x = 2;
@@ -7,7 +8,7 @@ describe("Authentication", () => {
         expect(sum).toBe(6);
     });
 });
-
+*/
 /*
 const { User } = require("../../src/app/models");
 
@@ -21,9 +22,36 @@ describe("Authentication", () => {
             password_hash: "1f2s31f3s12d1f23a3"
         });
 
-        console.log(user);
-
         expect(user.name).toBe("Elvis");
     });
 });
 */
+
+const request = require("supertest");
+const app = require("../../src/app");
+
+const { User } = require("../../src/app/models");
+const truncate = require("../utils/truncate");
+
+describe("Authentication", () => {
+    beforeEach(async () => {
+        await truncate();
+    });
+
+    it("should authenticate with valid credential", async () => {
+        const user = await User.create({
+            name: "Elvis", 
+            email: "elvis@elvis.com", 
+            password: "1f2s31f3s12d1f23a3"
+        });
+
+        const response = await request(app)
+        .post("/sessions")
+        .send({
+            email: user.email,
+            password: "123456"
+        });
+
+        expect(response.status).toBe(200);
+    })
+});
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
 const routes = require("express").Router();
+const SessionController = require("./app/controllers/SessionController");
+
+
+routes.post("/sessions", SessionController.store);
 
 // Definicão de rotas
 module.exports = routes;
